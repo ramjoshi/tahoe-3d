2a25ab2dcc10c223d0f159ce0e86eb4825e4d87b
Ram Joshi <ram@ramjoshi.org>
2014-04-22 21:47:54 -0700

    Added png texture

--- app/scripts/viz/views/terrain.js
+++ app/scripts/viz/views/terrain.js
@@ -49,7 +49,7 @@ define([
 
     initMaterials: function() {
       this._terrainMaterial = new THREE.MeshPhongMaterial({
-        map: THREE.ImageUtils.loadTexture('images/tahoe_texture.jpg')
+        map: THREE.ImageUtils.loadTexture('images/tahoe_texture.png')
       });
     },
 
